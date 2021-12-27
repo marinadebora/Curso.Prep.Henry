e0f3656c9d3226afb65edb9fa9c7fc148537fccb
marinadebora <m_deby_c@ hotmail.com>
2021-12-27 03:31:42 -0300

    first commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,21 +5,35 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x >= y){
+  return x;}
+  return y;
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
-}
-  
+  if (edad >= 18 ) {
+    return "Allowed"
+      }
+      else {
+    return "Not allowed" }
+}  
 function conection(status) {
   //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
   //Cuando el estado es igual a 1, el usuario está "Online"
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
-}
+  if (status === 1 ) {
+  return ("Online")
+  }
+    if (status === 2){
+     return ("Away")}
+       else { 
+      return ("Offline")}
+      }
 
 function saludo(idioma) {
   // Devuelve un saludo en tres diferentes lenguajes:
@@ -28,6 +42,14 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma === "aleman"){
+    return "Guten Tag!"}
+    if (idioma ==="mandarin"){
+    return "Ni Hao!"}
+    if (idioma ==="ingles"){
+     return "Hello!"}
+    
+    return "Hola!"
 }
 
 function colors(color) {
