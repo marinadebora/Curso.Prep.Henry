c0427458eb0bcb53d0b742d8159ae329479d6afd
marinadebora <m_deby_c@ hotmail.com>
2021-12-27 01:54:41 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -236,10 +236,14 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+if (letra === a || e || i || o || u);{
+  console.log ("es vocal");
+  console.log ("dato incorrecto");
 }
-
-
+  if (letra == 2) {
+    console.log ("dato incorrecto");
+    
+  }
 
 // No modificar nada debajo de esta línea
 // --------------------------------
