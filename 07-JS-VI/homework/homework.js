13bc57db98aaee353eea716c8357f4f8db06bfc8
marinadebora <m_deby_c@ hotmail.com>
2022-01-02 00:37:03 -0300

    How do I know if I do it right?

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,44 +4,76 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
-}
+  var nuevonombre=nombre.map(function(elemento){
+    return elemento.capitalize();
+  },
+
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
-}
+   cb();
+},
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
-}
+  return cb(n1,n2);
+},
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
-  // No es necesario devolver nada
+  // No es necesario devolver nada 
   //Tu código:
-}
+  var sumareduce= numeros.reduce (function( suma1, suma2){
+    return cb (suma1 + suma2); 0
+      
+  },
+  cb (sumareduce)
+,
 
-function forEach(array, cb) {
+    
+
+function forEach (array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
-}
+  array.forEach(function(elemento, indice){
+    cb= elemento})
+  
+},
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-}
+ 
+ 
+ let arr= array.map( function(){ arr.lenght=== array.lenght});
+ console.log(arr);
+ }
+ 
+
+
+
+,
+
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
-}
+  let arr= array.filter(function(array){
+    array[0]=== a
+  })
+   return arr
+
+
+  })
+},
 
 // No modificar nada debajo de esta línea
 // --------------------------------
@@ -54,4 +86,4 @@ module.exports = {
   forEach,
   map,
   filter
-};
+}
